3b5577c699a14a455f4f3498e5aceb49a392dd5b
pavel <dongod@bk.ru>
2022-12-28 21:53:13 +0400

    загрузил код

---
+++ code/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+int maxnum = 3;
+
+string[] FillArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= maxnum)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    foreach (string el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+
+string[] shortArray = FillArray(array);
+PrintArray(shortArray);
